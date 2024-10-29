7aba635ac0dd29ee3f4cd0c09bcba4da0122228f
diamondbusiness <tungnd21896@gmail.com>
2024-10-29 19:14:38 +0700

    123

--- main.js
+++ main.js
@@ -4,15 +4,11 @@ document.querySelector(".btn").addEventListener("click", function() {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Select the "Thêm vào giỏ hàng" button inside gemmaBagModal
     const addToCartButton = document.querySelector('.add-to-cart-btn');
     
-    // Add a click event listener to the button
     addToCartButton.addEventListener('click', function () {
-        // Hide the gemmaBagModal
         $('#gemmaBagModal').modal('hide');
 
-        // Show the signInModal after gemmaBagModal is closed
         $('#gemmaBagModal').on('hidden.bs.modal', function () {
             $('#signInModal').modal('show');
         });
